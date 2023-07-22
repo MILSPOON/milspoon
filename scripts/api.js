361bc9512294ef5f3fbad2f653ba491a6f39850b
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-07-22 22:57:01 +0900

    추가: api불러오기, 기본 틀 제작

---
+++ scripts/api.js
@@ -0,0 +1,7 @@
+const apiUrl = `https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D`
+
+fetch(apiUrl)
+    .then(res => res.json())
+	.then(myJson => {
+        console.log(myJson);
+})
