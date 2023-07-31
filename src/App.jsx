7914771958062a66cea90c4ca1466d002ae916d9
MaTemPong <dlwodnjsakxpackdl@gmail.com>
2023-07-31 15:46:53 +0900

    수정: 모달창 90% 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import './App.css'
 import './global.css'
-
+import { useState, useEffect } from 'react'
+import Modal from './components/Modal'
 function App () {
-  const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
+  // const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
 
-  fetch(apiUrl)
-    .then(res => res.json())
-    .then(myJson => {
-      console.log(myJson)
-    })
+  // fetch(apiUrl)
+  //   .then(res => res.json())
+  //   .then(myJson => {
+  //     console.log(myJson)
+  //   })
+  const [currentEventName, setCurrentEventName] = useState('미지정')
 
+  function clickMode (e) {
+    const modeName = e.target.textContent
+    setCurrentEventName(modeName)
+  }
+
+  useEffect(() => {
+    console.log(currentEventName)
+  }, [currentEventName])
   return (
     <div className="App">
+      <Modal modeName={currentEventName}></Modal>
       <div className="content">
         <div className="mainContent">
-          <div className="todayQuiz">
+          <div className="todayQuiz" onClick={clickMode}>
             <h1>오늘의 퀴즈!</h1>
           </div>
-          <div className="quiz">
+          <div className="quiz" onClick={clickMode}>
             <h1>전체 퀴즈!</h1>
           </div>
         </div>
