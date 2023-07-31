f5c763b0af60b459202175a5a55c91d6f4b0676b
gwongibeom <k201gun@kakao.com>
2023-07-31 09:59:24 +0900

    set tailwindcss

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ function App () {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </header>
     </div>
   )
