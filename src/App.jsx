5b39cf845cd98a59b69c7dc245cc2ffe7894d4a2
MaTemPong <dlwodnjsakxpackdl@gmail.com>
2023-07-31 12:59:46 +0900

    추가: 메인화면 구도 잡음

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg'
 import './App.css'
+import './global.css'
 
 function App () {
   const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
@@ -12,23 +12,10 @@ function App () {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </header>
+      <div className="mainContent">
+        <h1>테스트</h1>
+        <p>테스트</p>
+      </div>
     </div>
   )
 }
