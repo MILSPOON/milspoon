e843642435565488f2bc9f4dca22cdb7eaafcaa6
gwongibeom <k201gun@kakao.com>
2023-07-31 09:27:44 +0900

    set eslint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
   const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
 
-fetch(apiUrl)
-  .then(res => res.json())
-  .then(myJson => {
-    console.log(myJson)
-  })
+  fetch(apiUrl)
+    .then(res => res.json())
+    .then(myJson => {
+      console.log(myJson)
+    })
 
   return (
     <div className="App">
@@ -27,7 +27,7 @@ fetch(apiUrl)
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
