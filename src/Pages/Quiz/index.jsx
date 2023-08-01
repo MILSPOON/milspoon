8a8178de160ecd529965c4a01ad3e8ef0b578002
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-08-02 02:39:00 +0900

    개편: 코드 리펙토링 및 퀴즈 버그 수정

--- src/Pages/Quiz/index.jsx
+++ src/Pages/Quiz/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './style.css'
 
 function Quiz () {
@@ -6,60 +6,41 @@ function Quiz () {
   const [realAnswer, setRealAnswer] = useState('')
   const [answerNum, setAnswerNum] = useState(['', '', '', ''])
   const [isNext, setNext] = useState(false)
-  const [myJson, setMyJson] = useState({})
+
   async function api () {
-    if (isNext) {
-      console.log(myJson)
-      let randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
-      setQuestion(myJson.data[randomNum].설명)
-      const realAnswerNum = Math.round(Math.random() * 3)
-      setRealAnswer(myJson.data[realAnswerNum].표제어)
-      const newAnswerNum = [...answerNum]
-      for (let i = 0; i < 4; i++) {
-        randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
-        if (realAnswerNum === i) {
-          newAnswerNum[i] = realAnswer
-          console.log(realAnswer)
-        } else {
-          newAnswerNum[i] = myJson.data[randomNum].표제어
+    if (isNext) return
+    const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
+    await fetch(apiUrl)
+      .then(res => res.json())
+      .then(myJson => {
+        console.log(myJson)
+        let randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
+        const realAnswerNum = Math.round(Math.random() * 3)
+        setQuestion(myJson.data[randomNum].설명)
+        setRealAnswer(myJson.data[randomNum].표제어)
+
+        const newAnswerNum = [...answerNum]
+        for (let i = 0; i < 4; i++) {
+          if (realAnswerNum === i) {
+            newAnswerNum[i] = myJson.data[randomNum].표제어
+          } else {
+            newAnswerNum[i] = myJson.data[randomNum].표제어
+          }
+          randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
         }
-      }
-      setNext(true)
-      setAnswerNum(newAnswerNum)
-    } else {
-      const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
-      await fetch(apiUrl)
-        .then(res => res.json())
-        .then(resJson => {
-          setMyJson(resJson)
-        })
-    }
+        console.log(myJson.data[realAnswerNum].표제어)
+        setAnswerNum(newAnswerNum)
+      })
+    setNext(true)
   }
-  useEffect(() => {
-    console.log(isNext)
-    if (isNext) {
-      console.log(myJson)
-      let randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
-      setQuestion(myJson.data[randomNum].설명)
-      const realAnswerNum = Math.round(Math.random() * 3)
-      setRealAnswer(myJson.data[realAnswerNum].표제어)
-      const newAnswerNum = [...answerNum]
-      for (let i = 0; i < 4; i++) {
-        randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
-        if (realAnswerNum === i) {
-          newAnswerNum[i] = realAnswer
-          console.log(realAnswer)
-        } else {
-          newAnswerNum[i] = myJson.data[randomNum].표제어
-        }
-      }
-      setAnswerNum(newAnswerNum)
-    }
-  }, isNext)
   api()
 
+  console.log(realAnswer)
   return (
     <div className='quizSite'>
+      <div className="timer">
+
+      </div>
       <div className="questionContent">
         <h3 className='question'>{question}</h3>
       </div>
