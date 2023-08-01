ed6ff358304fa62cb2aa1f23f0d531acd18de4e8
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-08-02 00:40:01 +0900

    추가: 퀴즈 랜덤 문제 구현

--- src/Pages/Quiz/index.jsx
+++ src/Pages/Quiz/index.jsx
@@ -1,24 +1,73 @@
+import { useState, useEffect } from 'react'
 import './style.css'
 
 function Quiz () {
-  const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
-
-  fetch(apiUrl)
-    .then(res => res.json())
-    .then(myJson => {
+  const [question, setQuestion] = useState('')
+  const [realAnswer, setRealAnswer] = useState('')
+  const [answerNum, setAnswerNum] = useState(['', '', '', ''])
+  const [isNext, setNext] = useState(false)
+  const [myJson, setMyJson] = useState({})
+  async function api () {
+    if (isNext) {
       console.log(myJson)
-    })
+      let randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
+      setQuestion(myJson.data[randomNum].설명)
+      const realAnswerNum = Math.round(Math.random() * 3)
+      setRealAnswer(myJson.data[realAnswerNum].표제어)
+      const newAnswerNum = [...answerNum]
+      for (let i = 0; i < 4; i++) {
+        randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
+        if (realAnswerNum === i) {
+          newAnswerNum[i] = realAnswer
+          console.log(realAnswer)
+        } else {
+          newAnswerNum[i] = myJson.data[randomNum].표제어
+        }
+      }
+      setNext(true)
+      setAnswerNum(newAnswerNum)
+    } else {
+      const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
+      await fetch(apiUrl)
+        .then(res => res.json())
+        .then(resJson => {
+          setMyJson(resJson)
+        })
+    }
+  }
+  useEffect(() => {
+    console.log(isNext)
+    if (isNext) {
+      console.log(myJson)
+      let randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
+      setQuestion(myJson.data[randomNum].설명)
+      const realAnswerNum = Math.round(Math.random() * 3)
+      setRealAnswer(myJson.data[realAnswerNum].표제어)
+      const newAnswerNum = [...answerNum]
+      for (let i = 0; i < 4; i++) {
+        randomNum = Math.round(Math.random() * Object.keys(myJson.data).length)
+        if (realAnswerNum === i) {
+          newAnswerNum[i] = realAnswer
+          console.log(realAnswer)
+        } else {
+          newAnswerNum[i] = myJson.data[randomNum].표제어
+        }
+      }
+      setAnswerNum(newAnswerNum)
+    }
+  }, isNext)
+  api()
+
   return (
     <div className='quizSite'>
       <div className="questionContent">
-        <h3 className='question'>(1) 네트워크를 효율적으로 운용하기 위해 여러 개의 에이전트를 두고 매니저는 필요 시 에이전트에 게 정보를 요청하면 에이전트는 응답하는 방식의 TCP/ IP 망 관리 프로토콜. TCP/IP의 단순형 게이트웨이 모니터링 프로토콜(SGMP)을 바탕으로 개발되었으며, 개방형 시스템 간 상호 접속(OSI)의 망 공통 관리 정보 프로토콜CMIP)에 대응한다. 요구와 응답의 2가지 기능을 사용하여 망 관리 정보를 수집, 관리한다. 1988년에 RFC 1157로 간이 망 관리 프로토콜(SNMP) 표준이 발표되었으며, 1991년에 개정판인 SNMP2가 개발되어 SNMP2에 대응하는 제품도 판매되고 있다. (2) SNMP와 밀접한 관계가 있는 망 관리 정보 베이스(MIB)의 총칭. ☜ SNMP(Simple Network Management Protocol)</h3>
+        <h3 className='question'>{question}</h3>
       </div>
       <div className="quizContent">
-        <button className="answerBtn">버튼1</button>
-        <button className="answerBtn">버튼2</button>
-        <button className="answerBtn">버튼3</button>
-        <button className="answerBtn">버튼4</button>
-        <button className="answerBtn">버튼5</button>
+        <button className="answerBtn">{answerNum[0]}</button>
+        <button className="answerBtn">{answerNum[1]}</button>
+        <button className="answerBtn">{answerNum[2]}</button>
+        <button className="answerBtn">{answerNum[3]}</button>
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import reportWebVitals from './reportWebVitals'
 import Router from './Router'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
     <Router />
-  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
