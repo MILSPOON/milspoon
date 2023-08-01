135b5bebac9af57a7eb5d73b109946e8ad64548c
sw347 <sdh230310@sdh.hs.kr>
2023-08-01 09:41:51 +0900

    사전 제작

--- src/Pages/Dictionary/index.jsx
+++ src/Pages/Dictionary/index.jsx
@@ -1,7 +1,16 @@
+import "./style.css"
+
 function Dictionary () {
     return(
-        
-
+        <div>
+            <div className="menubar">
+                <p>용어 사전</p>
+            </div>
+            <div className="table">
+                <ul>
+                </ul>
+            </div>
+        </div>
     )
 }
 
