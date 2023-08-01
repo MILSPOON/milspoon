1ca52dee6a977cc4640fa751388d7952c72fbc67
MaTemPong <dlwodnjsakxpackdl@gmail.com>
2023-08-01 12:42:39 +0900

    추가: 메인페이지 디자인 개편

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './global.css'
 import { useState, useEffect } from 'react'
 import Modal from './components/Modal'
 import { Link } from 'react-router-dom'
+import chartIcon from './media/3dChartIcon.png'
 function App () {
   // const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
 
@@ -22,7 +23,10 @@ function App () {
   }, [currentEventName])
   return (
     <div className="App">
-      <Modal modeName={currentEventName}></Modal>
+      <Modal modeName={currentEventName} setModeName={setCurrentEventName}></Modal>
+      <div className='infoBox'>
+        <img src={chartIcon} width='250' className='chart'/>
+      </div>
       <div className="content">
         <div className="mainContent">
           <div className="todayQuiz" onClick={clickMode}>
