fc3fb1caf7c661106ac1d886045204d0a91c09d3
sw347 <sdh230310@sdh.hs.kr>
2023-08-02 14:52:19 +0900

    사전 api완료

--- src/Pages/Dictionary/index.jsx
+++ src/Pages/Dictionary/index.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
 import styles from './style.module.css'
 
-import SAMPLE_LIST from '../../data/sampleDictionary.json'
+// import SAMPLE_LIST from '../../data/sampleDictionary.json'
+import { Fragment, useCallback, useEffect, useState } from 'react'
 
 // const obj = {
 
@@ -21,24 +22,54 @@ const ListItem = (props) => {
       <a href={`/dictionary/detail?number=${props.index}`}>
       <div className={styles.dictionary_info}>
         <span className={styles.number}>{props.index}</span>
+        <div className={styles.dictionary_content}>
+
         <ul className={styles.dictionary_categories}>
           {props.category.split('>').map((word, index) => (
-            <>
+            <Fragment key={props.category + '_' + index}>
               {index !== 0 && <span>{'>'}</span>}
             <span key={word + index} className={styles.dictionary_category}>
               {word}
             </span>
-            </>
+            </Fragment>
           ))}
         </ul>
+      <p className={styles.dictionary_entry}>{props.title}</p>
+        </div>
       </div>
-      <p>{props.title}</p>
       </a>
     </li>
   )
 }
 
 function Dictionary () {
+  const [list, setList] = useState(null)
+
+  const [totalCount, setTotalCount] = useState(null)
+
+  const loadMorePage = useCallback((pageNum) => {
+    fetch(`https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=${pageNum}&perPage=100&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D`, {
+      method: 'GET'
+    }).then(res => res.json())
+      .then(data => {
+        setList(list => {
+          if (list.length && Math.floor(list.length / 100) === pageNum) return list
+          return [...list, ...data.data]
+        })
+        setTotalCount(data.totalCount)
+      })
+  }, [])
+
+  useEffect(() => {
+    fetch('https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=100&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D', {
+      method: 'GET'
+    }).then(res => res.json())
+      .then(data => {
+        setList(data.data)
+        setTotalCount(data.totalCount)
+      })
+  }, [])
+
   return (
     <div>
       <div className={styles.menubar}>
@@ -50,17 +81,23 @@ function Dictionary () {
           <path d="M18.4636 4.95413C18.6576 5.20489 18.882 5.41284 19.1368 5.57798C19.3973 5.737 19.6383 5.81651 19.8599 5.81651C20.1979 5.81651 20.4722 5.73089 20.6828 5.55963C20.8989 5.38838 21.0069 5.1682 21.0069 4.89908C21.0069 4.75229 20.9543 4.62997 20.849 4.53211C20.7492 4.42813 20.6218 4.34557 20.4667 4.2844C20.3171 4.22324 20.101 4.14679 19.8184 4.05505C19.3862 3.92049 19.0316 3.78593 18.7545 3.65138C18.483 3.51682 18.2475 3.31499 18.0481 3.04587C17.8486 2.77676 17.7489 2.42202 17.7489 1.98165C17.7489 1.59633 17.8403 1.25382 18.0231 0.954129C18.2115 0.654435 18.4719 0.422018 18.8044 0.256881C19.1369 0.0856269 19.5164 0 19.943 0C20.3198 0 20.7022 0.100918 21.09 0.302753C21.4834 0.504588 21.7992 0.767584 22.0375 1.09174L21.1149 1.89908C20.9598 1.69113 20.7742 1.51988 20.5581 1.38532C20.342 1.25076 20.1453 1.18349 19.968 1.18349C19.6854 1.18349 19.4555 1.25688 19.2781 1.40367C19.1008 1.55046 19.0122 1.74006 19.0122 1.97248C19.0122 2.18654 19.1119 2.35474 19.3114 2.47706C19.5109 2.59939 19.8211 2.73395 20.2423 2.88073C20.6634 3.02752 21.0097 3.17125 21.2812 3.31193C21.5527 3.44648 21.7854 3.64526 21.9793 3.90826C22.1733 4.16514 22.2702 4.49541 22.2702 4.89908C22.2702 5.30887 22.1677 5.67278 21.9627 5.99083C21.7632 6.30887 21.4806 6.55657 21.1149 6.73394C20.7548 6.91131 20.3448 7 19.8849 7C19.4693 7 19.0399 6.88685 18.5966 6.66055C18.1533 6.42813 17.7932 6.1315 17.5161 5.77064L18.4636 4.95413Z" fill="white"/>
           <path d="M30 6C30 6.55228 29.5523 7 29 7C28.4477 7 28 6.55228 28 6C28 5.44772 28.4477 5 29 5C29.5523 5 30 5.44772 30 6Z" fill="#D0FF00"/>
         </svg>
-        <p>용어 사전</p>
+        <div className={styles.circle}></div>
       </div>
         <ul className={styles.dictionary_list}>
-          {SAMPLE_LIST.map((item) => (
+          {list?.map((item) => (
             <ListItem
               key={item.순번}
               index={item.순번}
               title={item.표제어}
               category={item.분류}
             />
-          ))}
+          )) || <h1>로딩 중</h1>}
+          {list && list.length < totalCount && (
+            <button className={styles.loadButton} onClick={() => {
+              loadMorePage(list.length / 100 + 1)
+              loadMorePage(list.length / 100 + 1)
+            }}>추가 로드</button>
+          ) }
         </ul>
     </div>
   )
--- src/Pages/DictionaryDetail/index.jsx
+++ src/Pages/DictionaryDetail/index.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
 import './style.css'
 
-import SAMPLE_LIST from '../../data/sampleDictionary.json'
 import { useSearchParams } from 'react-router-dom'
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 function Dictionary () {
   const [searchParams] = useSearchParams()
 
   const number = useMemo(() => searchParams.get('number'), [])
 
-  const detailData = useMemo(() => number ? SAMPLE_LIST[number] : undefined, [number])
+  const [detailData, setDetailData] = useState(null)
+
+  useEffect(() => {
+    if (!number) return
+    fetch('https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D', {
+      method: 'GET'
+    }).then(res => res.json())
+      .then(data => setDetailData(data.data[number]))
+  }, [number])
 
   if (number) {
     return (
@@ -24,7 +31,7 @@ function Dictionary () {
         <path d="M18.4636 4.95413C18.6576 5.20489 18.882 5.41284 19.1368 5.57798C19.3973 5.737 19.6383 5.81651 19.8599 5.81651C20.1979 5.81651 20.4722 5.73089 20.6828 5.55963C20.8989 5.38838 21.0069 5.1682 21.0069 4.89908C21.0069 4.75229 20.9543 4.62997 20.849 4.53211C20.7492 4.42813 20.6218 4.34557 20.4667 4.2844C20.3171 4.22324 20.101 4.14679 19.8184 4.05505C19.3862 3.92049 19.0316 3.78593 18.7545 3.65138C18.483 3.51682 18.2475 3.31499 18.0481 3.04587C17.8486 2.77676 17.7489 2.42202 17.7489 1.98165C17.7489 1.59633 17.8403 1.25382 18.0231 0.954129C18.2115 0.654435 18.4719 0.422018 18.8044 0.256881C19.1369 0.0856269 19.5164 0 19.943 0C20.3198 0 20.7022 0.100918 21.09 0.302753C21.4834 0.504588 21.7992 0.767584 22.0375 1.09174L21.1149 1.89908C20.9598 1.69113 20.7742 1.51988 20.5581 1.38532C20.342 1.25076 20.1453 1.18349 19.968 1.18349C19.6854 1.18349 19.4555 1.25688 19.2781 1.40367C19.1008 1.55046 19.0122 1.74006 19.0122 1.97248C19.0122 2.18654 19.1119 2.35474 19.3114 2.47706C19.5109 2.59939 19.8211 2.73395 20.2423 2.88073C20.6634 3.02752 21.0097 3.17125 21.2812 3.31193C21.5527 3.44648 21.7854 3.64526 21.9793 3.90826C22.1733 4.16514 22.2702 4.49541 22.2702 4.89908C22.2702 5.30887 22.1677 5.67278 21.9627 5.99083C21.7632 6.30887 21.4806 6.55657 21.1149 6.73394C20.7548 6.91131 20.3448 7 19.8849 7C19.4693 7 19.0399 6.88685 18.5966 6.66055C18.1533 6.42813 17.7932 6.1315 17.5161 5.77064L18.4636 4.95413Z" fill="white"/>
         <path d="M30 6C30 6.55228 29.5523 7 29 7C28.4477 7 28 6.55228 28 6C28 5.44772 28.4477 5 29 5C29.5523 5 30 5.44772 30 6Z" fill="#D0FF00"/>
       </svg>
-        <p>용어 상세</p>
+      <div className="circle"></div>
       </div>
       <div className='dictionary_word'>
         <div className='word_classification'>분류:{detailData?.분류}</div>
@@ -32,6 +39,7 @@ function Dictionary () {
         <div className='word_explane'>설명:{detailData?.설명}</div>
         <hr />
         <div className='word_whence'>출처:{detailData?.출처}, {detailData?.출처1}</div>
+        <hr />
       </div>
     </div>
     )
