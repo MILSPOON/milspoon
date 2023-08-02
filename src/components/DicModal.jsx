9f918d74c84aaee344e98689aa64ea58ffa5d445
sw347 <sdh230310@sdh.hs.kr>
2023-08-02 10:00:37 +0900

    pull받기위해서 commit합니다

---
+++ src/components/DicModal.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+//import { useState } from 'react'
+
+function DicModal (props) {
+  //const [isShowModal, setIsShowModal] = useState(false)
+  return (
+        <div className='bg' >
+            <div className='modal'>
+            </div>
+        </div>
+  )
+}
+
+export default DicModal
