b5dbbc5c4964b4ec01831cbdd0ed0080f8d5c8ef
MaTemPong <dlwodnjsakxpackdl@gmail.com>
2023-08-02 14:52:10 +0900

    추가: 타이머 시스템 90% 완료 및 점수 시스템 완성

--- src/Pages/Quiz/index.jsx
+++ src/Pages/Quiz/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './style.css'
 
 function Quiz () {
@@ -7,8 +7,37 @@ function Quiz () {
   const [answerNum, setAnswerNum] = useState(['', '', '', ''])
   const [isNext, setNext] = useState(false)
 
+  const maxTime = 60
+  const [time, setTime] = useState(maxTime)
   const [score, setScore] = useState(0)
 
+  const [percentTime, setPercentTime] = useState(100)
+
+  function useInterval (callback, delay) {
+    const savedCallback = useRef() // 최근에 들어온 callback을 저장할 ref를 하나 만든다.
+
+    useEffect(() => {
+      savedCallback.current = callback // callback이 바뀔 때마다 ref를 업데이트 해준다.
+    }, [callback])
+
+    useEffect(() => {
+      function tick () {
+        savedCallback.current() // tick이 실행되면 callback 함수를 실행시킨다.
+      }
+      if (delay !== null) { // 만약 delay가 null이 아니라면
+        const id = setInterval(tick, delay) // delay에 맞추어 interval을 새로 실행시킨다.
+        return () => clearInterval(id) // unmount될 때 clearInterval을 해준다.
+      }
+    }, [delay]) // delay가 바뀔 때마다 새로 실행된다.
+  }
+  if (time >= 0) {
+    useInterval(() => {
+      setTime(time - 0.1)
+      setPercentTime(time / maxTime * 100)
+      console.log(time)
+      console.log(percentTime)
+    }, 100)
+  }
   async function api () {
     if (isNext) return
     const apiUrl = 'https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D'
@@ -36,13 +65,11 @@ function Quiz () {
     setNext(true)
   }
 
-  console.log(realAnswer)
-
   function answerCheck (e) {
     if (!isNext) return
     if (e.target.textContent === realAnswer) {
       setNext(false)
-      setScore(score + 1)
+      setScore(score + 100)
     } else {
       console.log('오답')
     }
@@ -59,7 +86,7 @@ function Quiz () {
           <div className="info">
             <div className="timer">
               <div className="timerRange">
-                <div className="fillRange"></div>
+                <div className="fillRange" style={{ width: `${percentTime}%` }}></div>
               </div>
             </div>
               <h1 className="scoreCount">
