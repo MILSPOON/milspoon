33cdb2e79585436033684d83ef924711218a0ab5
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-08-04 03:26:32 +0900

    회원가입 틀 제작(프론트)

--- src/Pages/AccountCreate/index.jsx
+++ src/Pages/AccountCreate/index.jsx
@@ -1,9 +1,22 @@
 import './style.css'
 import MotionLogo from '../../components/MotionLogo'
+import MilHeader from '../../components/MilHeader'
+import Nav from '../../components/Nav'
 
 const AccountCreate = () => {
   return (
-    <MotionLogo></MotionLogo>
+    <div className='accountCreate'>
+      <MilHeader/>
+      <MotionLogo></MotionLogo>
+      <div className="accountCreate">
+        <h1>회원가입</h1>
+        <input type='text' name='userId' placeholder='이름...'/>
+        <input type='text' name='userPw' placeholder='아이디...'/>
+        <input type='text' name='userPws' placeholder='비밀번호...'/>
+        <button>회원가입</button>
+      </div>
+      <Nav></Nav>
+    </div>
   )
 }
 
