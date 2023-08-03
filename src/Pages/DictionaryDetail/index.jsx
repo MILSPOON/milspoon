8525eee1b0d3f05cde680b0a18a23844ccce435c
sw347 <sdh230310@sdh.hs.kr>
2023-08-04 02:40:55 +0900

    사전 제작 완료

--- src/Pages/DictionaryDetail/index.jsx
+++ src/Pages/DictionaryDetail/index.jsx
@@ -2,7 +2,7 @@
 import './style.css'
 
 import { useSearchParams } from 'react-router-dom'
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import Nav from '../../components/Nav'
 import MilHeader from '../../components/MilHeader'
 
@@ -10,16 +10,19 @@ function Dictionary () {
   const [searchParams] = useSearchParams()
 
   const number = useMemo(() => searchParams.get('number'), [])
+  const category = useMemo(() => searchParams.get('category'), [])
+  const deso = useMemo(() => searchParams.get('deso'), [])
+  const c = useMemo(() => searchParams.get('c'), [])
+  const c1 = useMemo(() => searchParams.get('c1'), [])
+  // const [detailData, setDetailData] = useState(null)
 
-  const [detailData, setDetailData] = useState(null)
-
-  useEffect(() => {
-    if (!number) return
-    fetch('https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D', {
-      method: 'GET'
-    }).then(res => res.json())
-      .then(data => setDetailData(data.data[number - 1]))
-  }, [number])
+  // useEffect(() => {
+  //   if (!number) return
+  //   fetch('https://api.odcloud.kr/api/15089127/v1/uddi:858d65b9-ce3c-4a68-94b2-989ac92385c9?page=1&perPage=2000&serviceKey=BP1Ko40djjq%2FdGO47n5u7rYb2mIGEFcqZte4zYQihF59HR99CJxkSuEEwTXhErxIX1apz0eudJwcp9HowwFSSA%3D%3D', {
+  //     method: 'GET'
+  //   }).then(res => res.json())
+  //     .then(data => setDetailData(data.data[number - 1]))
+  // }, [number])
 
   if (number) {
     return (
@@ -28,16 +31,16 @@ function Dictionary () {
       <div className='dictionary_word'>
         <div className='word_classification'>
           <h3 className='subheading'>분류</h3>
-          {detailData?.분류}</div>
+          {category}</div>
         <hr />
         <div className='word_explane'>
           <h3 className='subheading'>설명</h3>
-          {detailData?.설명}
+          {deso}
           </div>
         <hr />
         <div className='word_whence'>
           <h3 className='subheading'>출처</h3>
-          {detailData?.출처}, {detailData?.출처1}</div>
+          {c}, {c1}</div>
         <hr />
       </div>
       <Nav/>
